4e14067ce9ec4ef5989f32d53409a4daff558708
danikaze <danikaze@gmail.com>
2021-04-18 14:53:00 +0900

    Update to use the new channel points rewards url

--- src/utils/detect-page.ts
+++ src/utils/detect-page.ts
@@ -3,7 +3,7 @@ export type PageType = 'channel-point-rewards' | 'view' | undefined;
 export function detectPage(): PageType {
   const pathname = location.pathname;
 
-  if (/community\/channel-points\/rewards$/.test(pathname)) {
+  if (/viewer-rewards\/channel-points\/rewards$/.test(pathname)) {
     return 'channel-point-rewards';
   }
 
